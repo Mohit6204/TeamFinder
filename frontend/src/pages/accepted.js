87bdc3b562b9c1b968f291a14cebcdb47b132f80
Mohit Kumar Mishra <mishramohit6204@gmail.com>
2023-10-28 22:33:27 +0530

    fix kar diya elvish bhai ne

--- frontend/src/pages/accepted.js
+++ frontend/src/pages/accepted.js
@@ -42,8 +42,9 @@ function Accepted() {
             <Load />
          </div>
       </> :
-         myTeam.length ? <>
-               <div className=" flex my-4 flex-col mt-10 min-h-screen">
+         myTeam.length ? <div className="flex min-h-screen w-full">
+            <div className="  my-6 py-2 px-8 bg-slate-100 dark:bg-neutral-900 w-full">
+               <div className="grid w-full xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
                   {myTeam.map((team) => (
                      <div className="dark:bg-black hover:dark:shadow-slate-600 shadow-md flex flex-1 flex-col rounded-2xl bg-white m-6 hover:shadow-xl hover:m-5 duration-200 cursor-pointer p-3 hover:p-4" onClick={() => handleView(team)}>
                         <div className="py-2">
@@ -65,7 +66,8 @@ function Accepted() {
 
                   ))}
                </div>
-            </>: <>
+            </div>
+         </div> : <>
             <div className=" flex justify-center p-4 h-screen items-center flex-col dark:text-slate-300">
                <h1 className=" text-xl font-medium">Sorry, you are not added to any team yet.</h1>
                <div className="py-2 hover:text-blue-600 cursor-pointer" onClick={() => navigate("/Pending")}>Your Requests</div>
